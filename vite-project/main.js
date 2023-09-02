200033702edc66d810d41d69c448f1993e8f14d0
shota <shota19830706@gmail.com>
2023-09-02 10:19:20 +0900

    Viteを使ったファイルを作成しました

---
+++ vite-project/main.js
@@ -0,0 +1,10 @@
+import "./style.css";
+
+// Splide
+import Splide from "@splidejs/splide";
+import "@splidejs/splide/css";
+import "@splidejs/splide/css/skyblue";
+import "@splidejs/splide/css/sea-green";
+import "@splidejs/splide/css/core";
+
+new Splide(".splide").mount();
